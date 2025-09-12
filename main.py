12c1d7a165ca7dbd2f159fe50faed9ad89bd3e5b
aryan singh chandel <2k22.cse.32661@gmail.com>
2025-09-12 16:28:51 +0530

    hhh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import Baseodel
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
---
+++ models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, DateTime, Boolean
+from datetime import datetime
+from database import Base
+
+class ToDo(Base):
+    __tablename__ = "todos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    description = Column(String, nullable=True)
+    completed = Column(Boolean, default=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
